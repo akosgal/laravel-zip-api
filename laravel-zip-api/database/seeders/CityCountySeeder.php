642cd3da2b22be0f323e210c78b1afc0250682de
akosgal <gal.akos.2005@gmail.com>
2025-09-29 10:54:40 +0200

    cities, counties table populated

--- laravel-zip-api/database/seeders/CityCountySeeder.php
+++ laravel-zip-api/database/seeders/CityCountySeeder.php
@@ -3,10 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\County;
+use App\Models\City;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CountySeeder extends Seeder
+class CityCountySeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -18,6 +19,7 @@ class CountySeeder extends Seeder
         if (file_exists($filePath))
         {
             $lines = file($filePath, FILE_IGNORE_NEW_LINES);
+            $cities = [];
             $counties = [];
             for ($i=5; $i < count($lines); $i++) 
             {
@@ -26,12 +28,25 @@ class CountySeeder extends Seeder
                 {
                     $counties[] = $data[2];
                 }
+                if ($data[0] != "")
+                {
+                    $cities[] = [$data[0], $data[1], array_search($data[2], $counties) + 1];
+                }
             }
             for ($i = 0; $i < count($counties); $i++)
             {
                 County::create([
                     'id' => $i + 1,
-                    'name' => $counties[$i],
+                    'name' => $counties[$i]
+                ]);
+            }
+            for ($i=0; $i < count($cities); $i++)
+            { 
+                City::create([
+                    'id' => $i + 1,
+                    'postal_code' => $cities[$i][0],
+                    'name' => $cities[$i][1],
+                    'county_id' => $cities[$i][2]
                 ]);
             }
         } else
