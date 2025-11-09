f2a6527f3a3110676ea7bce6a6b3fcb6906ea9a4
akosgal <gal.akos.2005@gmail.com>
2025-11-09 18:40:05 +0100

    updated routes/api.php

--- laravel-zip-api/routes/api.php
+++ laravel-zip-api/routes/api.php
@@ -1,23 +1,25 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CityCountyController;
+use App\Http\Controllers\CountyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('counties', [CityCountyController::class, 'index']);
-Route::get('counties/{countyId}', [CityCountyController::class, 'countyName']);
-Route::get('counties/{countyId}/cities', [CityCountyController::class, 'show']);
-Route::get('counties/{countyId}/cities/{cityId}', [CityCountyController::class, 'cityInCounty']);
+Route::get('counties', [CountyController::class, 'index']);
+Route::get('counties/{countyId}', [CountyController::class, 'countyName']);
+Route::get('counties/{countyId}/cities', [CityController::class, 'show']);
+Route::get('counties/{countyId}/cities/{cityId}', [CityController::class, 'cityInCounty']);
 
 Route::middleware('auth:sanctum')->group(function() {
-    Route::post('counties', [CityCountyController::class, 'createCounty']);
-    Route::put('counties/{countyId}', [CityCountyController::class, 'updateCounty']);
-    Route::delete('counties/{countyId}', [CityCountyController::class, 'deleteCounty']);
-    Route::post('counties/{countyId}/cities', [CityCountyController::class, 'createCity']);
-    Route::put('counties/{countyId}/cities/{cityId}', [CityCountyController::class, 'updateCity']);
-    Route::delete('counties/{countyId}/cities/{cityId}', [CityCountyController::class, 'deleteCity']);
+    Route::post('counties', [CountyController::class, 'createCounty']);
+    Route::put('counties/{countyId}', [CountyController::class, 'updateCounty']);
+    Route::delete('counties/{countyId}', [CountyController::class, 'deleteCounty']);
+    Route::post('counties/{countyId}/cities', [CityController::class, 'createCity']);
+    Route::put('counties/{countyId}/cities/{cityId}', [CityController::class, 'updateCity']);
+    Route::delete('counties/{countyId}/cities/{cityId}', [CityController::class, 'deleteCity']);
 });
