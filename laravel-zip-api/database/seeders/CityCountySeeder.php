8bdef925fe1b80f1e9d0c035b9ce8448422d080b
akosgal <gal.akos.2005@gmail.com>
2025-11-09 18:45:05 +0100

    updated db seeder for city and county tables

--- laravel-zip-api/database/seeders/CityCountySeeder.php
+++ laravel-zip-api/database/seeders/CityCountySeeder.php
@@ -16,42 +16,41 @@ class CityCountySeeder extends Seeder
     {
         $filePath = storage_path('iranyitoszamok.csv');
 
-        if (file_exists($filePath))
-        {
-            $lines = file($filePath, FILE_IGNORE_NEW_LINES);
-            $cities = [];
-            $counties = [];
-            for ($i=5; $i < count($lines); $i++) 
-            {
-                $data = explode(';', $lines[$i]);
-                if (!in_array($data[2], $counties) && $data[2] != "")
-                {
-                    $counties[] = $data[2];
-                }
-                if ($data[0] != "")
-                {
-                    $cities[] = [$data[0], $data[1], array_search($data[2], $counties) + 1];
-                }
-            }
-            for ($i = 0; $i < count($counties); $i++)
-            {
-                County::create([
-                    'id' => $i + 1,
-                    'name' => $counties[$i]
-                ]);
-            }
-            for ($i=0; $i < count($cities); $i++)
-            { 
-                City::create([
-                    'id' => $i + 1,
-                    'postal_code' => $cities[$i][0],
-                    'name' => $cities[$i][1],
-                    'county_id' => $cities[$i][2]
-                ]);
-            }
-        } else
-        {
+        if (!file_exists($filePath)) {
             echo "File not found: $filePath\n";
+            return;
+        }
+
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES);
+        $cities = [];
+        $counties = [];
+        for ($i=5; $i < count($lines); $i++) 
+        {
+            $data = explode(';', $lines[$i]);
+            if (!in_array($data[2], $counties) && $data[2] != "")
+            {
+                $counties[] = $data[2];
+            }
+            if ($data[0] != "")
+            {
+                $cities[] = [$data[0], $data[1], array_search($data[2], $counties) + 1];
+            }
+        }
+        for ($i = 0; $i < count($counties); $i++)
+        {
+            County::create([
+                'id' => $i + 1,
+                'name' => $counties[$i]
+            ]);
+        }
+        for ($i=0; $i < count($cities); $i++)
+        { 
+            City::create([
+                'id' => $i + 1,
+                'postal_code' => $cities[$i][0],
+                'name' => $cities[$i][1],
+                'county_id' => $cities[$i][2]
+            ]);
         }
     }
 }
